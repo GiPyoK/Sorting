67a6802bc1a2d17f5273bcd1eeb6013a8089cbda
Gi Pyo Kim <gipyok@gmail.com>
2020-03-17 16:08:53 -0700

    Implemented Binary search resursive

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -41,6 +41,9 @@ def binary_search_recursive(arr, target, low, high):
   if len(arr) == 0:
     return -1 # array empty
   # TO-DO: add missing if/else statements, recursive calls
-
-arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
-print(binary_search(arr1,0))
+  if arr[middle] == target:
+    return middle
+  elif arr[middle] > target:
+    return binary_search_recursive(arr, target, low, middle - 1)
+  else:
+    return binary_search_recursive(arr, target, middle + 1, high)
